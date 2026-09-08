bb8702c17ea94907690dc2106241e9741fb81f88
popov09 <danilapopov16@yandex.ru>
2026-09-08 16:17:00 +0300

    проектная работа 10

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,26 +1,35 @@
-import { CSSProperties } from 'react';
+import { CSSProperties, useState } from 'react';
 import clsx from 'clsx';
 
 import { Article } from '../article/Article';
 import { ArticleParamsForm } from '../article-params-form/ArticleParamsForm';
-import { defaultArticleState } from './../../constants/articleProps';
+import {
+	defaultArticleState,
+	ArticleStateType,
+} from './../../constants/articleProps';
 
 import styles from './app.module.scss';
 
 export const App = () => {
+	const [pageSettings, setPageSettings] =
+		useState<ArticleStateType>(defaultArticleState);
+
 	return (
 		<main
 			className={clsx(styles.main)}
 			style={
 				{
-					'--font-family': defaultArticleState.fontFamilyOption.value,
-					'--font-size': defaultArticleState.fontSizeOption.value,
-					'--font-color': defaultArticleState.fontColor.value,
-					'--container-width': defaultArticleState.contentWidth.value,
-					'--bg-color': defaultArticleState.backgroundColor.value,
+					'--font-family': pageSettings.fontFamilyOption.value,
+					'--font-size': pageSettings.fontSizeOption.value,
+					'--font-color': pageSettings.fontColor.value,
+					'--container-width': pageSettings.contentWidth.value,
+					'--bg-color': pageSettings.backgroundColor.value,
 				} as CSSProperties
 			}>
-			<ArticleParamsForm />
+			<ArticleParamsForm
+				pageSettings={pageSettings}
+				setPageSettings={setPageSettings}
+			/>
 			<Article />
 		</main>
 	);
